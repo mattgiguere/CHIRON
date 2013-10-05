63e78626154ce96d6e778c87f77f79a706c5d373
Matt Giguere <matthew.giguere@gmail.com>
2013-10-04 20:52:22 -0400

    added airmass plot
    looked at the residuals to see the extent to which airmass plays a role.

--- EXPM/chi_em_calib.py
+++ EXPM/chi_em_calib.py
@@ -12,31 +12,10 @@ def mattdir():
 		mdir = '/home/matt/'
 	else:
 		mdir = ''
-
-def em_calib(star, mdir):
-	slogfn = mdir+'data/CHIRPS/starlogs/'+star+'log.dat'
-	sl = readsav(slogfn)
-	#to print the contents of the "OBJECT" tag:
-	#sl.starlog.object
-	snrall = sl.starlog.snrbp5500
-	emavgstr = sl.starlog.emavg
-	emnumsmpstr = sl.starlog.emnumsmp
-	#now use only the good ones, the values with velocities:
-	gd = np.where(sl.starlog.cmnvel != 0)[0]
-	#print gd
-	snr = np.zeros(len(gd))
-	emavg = np.zeros(len(gd))
-	emnumsmp = np.zeros(len(gd))
-	#print len(gd)
-	for i in range(len(gd)):
-		#print i
-		snr[i] = float(snrall[gd[i]])
-		emavg[i] = float(emavgstr[gd[i]])
-		emnumsmp[i] = float(emnumsmpstr[gd[i]])
-	emcts = emavg * emnumsmp
-	return emcts, snr
+	print "mdir is: "+mdir
+	return mdir
 	
-def plot_em(emcts, snr, order, mdir, star):
+def plot_em(emcts, snr, order, mdir, star, model):
 	simsize = 3
 	#clear the window
 	pl.clf()
@@ -45,17 +24,14 @@ def plot_em(emcts, snr, order, mdir, star):
 	pl.ylabel('SNR')
 	pl.plot(emcts/1e6, snr, 'bo', markersize=simsize)
 	print "max snr: "+str(max(snr))
-	model = model_em(emcts, snr, order)
 	pl.plot(emcts/1e6, model, 'ro', markersize=simsize)
-	
 	stddev = np.std(snr - model)
 	print "Standard Deviation: "+str(stddev)
 	pl.text(5, 150, 'Polynomial Order: '+str(order))
 	pl.text(5, 130, 'Standard Deviation: {:.3}'.format(stddev))
 	#pl.show()
-	pl.savefig(mdir+'projects/OTHER/NOTES/EXPM/'+star+'_emcts_snr_ord'+str(order)+'.eps')
+	pl.savefig(mdir+'projects/OTHER/NOTES/EXPM/em_cts_snr/'+star+'_emcts_snr_ord'+str(order)+'.eps')
 	
-
 def model_em(emcts, snr, ord):
 	z = np.polyfit(emcts, snr, ord)
 	print z
@@ -64,7 +40,62 @@ def model_em(emcts, snr, ord):
 		mod = mod + z[ord - i] * emcts**i
 	return mod
 
-star = '10700'
+def plot_airmass(snr, model, airmass, order, star):
+	gdam = np.where(airmass != 0)[0]
+	resids = np.zeros(len(gdam))
+	airmgd = np.zeros(len(gdam))
+	for i in range(len(gdam)):
+		resids[i] = snr[gdam[i]] - model[gdam[i]]
+		airmgd[i] = airmass[gdam[i]]
+	pl.clf()
+	pl.xlabel('Airmass')
+	pl.ylabel('SNR - Model')
+	pl.plot(airmgd, resids, 'ro')
+	#pl.show()
+	pl.savefig(mdir+'projects/OTHER/NOTES/EXPM/em_airmass/'+star+'_emcts_snr_ord'+str(order)+'.eps')
+	
+def em_calib(star, mdir):
+	slogfn = mdir+'data/CHIRPS/starlogs/'+star+'log.dat'
+	sl = readsav(slogfn)
+	#to print the contents of the "OBJECT" tag:
+	#sl.starlog.object
+	snrall = sl.starlog.snrbp5500
+	emavgstr = sl.starlog.emavg
+	emnumsmpstr = sl.starlog.emnumsmp
+	airmassall = sl.starlog.airmass
+	#now use only the good ones, the values with velocities:
+	gd = np.where(sl.starlog.cmnvel != 0)[0]
+	#print gd
+	snr = np.zeros(len(gd))
+	emavg = np.zeros(len(gd))
+	emnumsmp = np.zeros(len(gd))
+	airmass = np.zeros(len(gd))
+	#print len(gd)
+	for i in range(len(gd)):
+		print i, airmassall[gd[i]]
+		snr[i] = float(snrall[gd[i]])
+		emavg[i] = float(emavgstr[gd[i]])
+		emnumsmp[i] = float(emnumsmpstr[gd[i]])
+		if str(airmassall[gd[i]]).strip() != 'airmass' and str(airmassall[gd[i]]).strip() != '':
+			airmass[i] = float(airmassall[gd[i]])
+	emcts = emavg * emnumsmp
+	return emcts, snr, airmass
+
+##########################################################
+### BEGIN CODE HERE
+##########################################################
+
+#Figure out what computer you're on:
 mdir = mattdir()
-emcts, snr = em_calib(star, mdir, star)
-plot_em(emcts, snr, 3, mdir)
+
+#The polynomial fit for the SNR(emcts):
+order = 3
+
+#The star you want to plot:
+star = '22049'
+
+emcts, snr, airmass = em_calib(star, mdir)
+model = model_em(emcts, snr, order)
+
+plot_em(emcts, snr, order, mdir, star, model)
+plot_airmass(snr, model, airmass, order, star)
