13b2cd69be5ab5f7125a1bae3eba56d059853acf
Matt Giguere <matthew.giguere@gmail.com>
2014-04-22 21:27:11 -0400

    added syncraw

--- chi_syncraw.py
+++ chi_syncraw.py
@@ -3,6 +3,9 @@
 """
 Created on 2014-04-08T14:49:44
 """
+from __future__ import division, print_function
+import sys
+import subprocess
 
 __author__       = "Matt Giguere (github: @mattgiguere)"
 __maintainer__ = "Matt Giguere"
@@ -10,26 +13,39 @@ __email__         = "matthew.giguere@yale.edu"
 __status__         = " Development NOT(Prototype or Production)"
 __version__       = '0.0.1'
 
-from __future__ import division, print_function
-import sys
-
+"""PURPOSE: To sync raw data from /raw to /raw2"""
 def chi_syncraw(arg1):
-	"""PURPOSE: To sync raw data from /raw to /raw2"""
-  import subprocess
-  rdirs = subprocess.check_output(["ls","-1","/Volumes/epsilon/raw/mir7"])
-  pref = ['rsync', '-uva', '/Volumes/epsilon/raw/mir7/']
-  suf = ['/Volumes/kappa/raw2/mir7/']
-  if arg1 == 'test':
-    for i in rdirs:
-      print pref,i,'/ ',suv,i,'/'
+  rdir = subprocess.check_output(["ls","-1","/raw/mir7"])
+  rdirs = rdir.split()
+  pref = ['rsync', '-uva', '/raw/mir7/']
+  #rdirs = [x if x]
+  suf = '/raw2/mir7/'
+  for i in rdirs:
+    comm = pref[:-1]
+    comm.append(''.join(pref[-1]+i+'/'))
+    comm.append(suf+i+'/')
+    if arg1 == 'test':
+      #print(comm)
+      print(' '.join(comm))
+    if arg1 == 'exec':
+      print("***************************")
+      print("Now syncing "+i)
+      print(subprocess.check_output(comm))
+
 
 if __name__ == '__main__':
-    if len(sys.argv) != 1:
-        print('')
-        print('python chi_syncraw.py test OR exec')
-        print('')
-        sys.exit(2)
-
-    arg1 = str(sys.argv[1])
-
-    chi_syncraw(arg1)
+    if len(sys.argv) != 2:
+      print('')
+      print("***Invalid call to chi_syncraw***")
+      print("***Must be of the following form***")
+      print('python chi_syncraw.py test OR exec')
+      print("# of arguments actually passed in: ", len(sys.argv))
+      print("Arguments: ", sys.argv)
+      if len(sys.argv) > 1:
+          print(sys.argv[1])
+      print('')
+      sys.exit(2)
+    else:
+      print("Arguments: ", sys.argv)
+      arg1 = str(sys.argv[1])
+      chi_syncraw(arg1)
